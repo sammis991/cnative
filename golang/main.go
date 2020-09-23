a580d82c26b5b049d6475622f705e6305cde43ad
Mishra <sambit.mishra@nokia.com>
2020-09-23 12:51:33 +0530

    pushing doc expt

---
+++ golang/main.go
@@ -0,0 +1,34 @@
+package main
+import "os"
+import "os/exec"
+import "fmt"
+
+// docker         run image <cmd> <params>
+// go run main.go run   <cmd> <params>
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+
+	default:
+		panic("bad command")
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
+}
+
+func must(err error){
+	if err  != nil {
+		panic(err)
+	}
+}
